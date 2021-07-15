e86b73d858044ae0e9d647f1b53a31e9caca669a
Aleksey <aragorn_108@mail.ru>
2021-07-15 14:22:33 +0800

    0.94.1a

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  transpileDependencies: ["vuetify"],
-	outputDir: "../server/src/public",	
-	baseURL: 'https://umk.coko38.ru',
+  transpileDependencies: ['vuetify'],
+	outputDir: '../server/src/public',	
+	publicPath: 'https://umk.coko38.ru',
 	devServer: {
     proxy: 'http://localhost:3000'
   },
