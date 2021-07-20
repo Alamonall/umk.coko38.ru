6cfd6d6444f9846e19bdcbbb912dcdf464970458
Aleksey <aragorn_108@mail.ru>
2021-07-20 14:21:28 +0800

    0.98a
    билд

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -4,7 +4,6 @@ import store from '../store'
 
 export default () => {
 	return axios.create({
-		baseURL: 'http://localhost:3000',
 		headers: {
 			Authorization: `Bearer ${store.state.token}`,
 		},
--- client/vue.config.js
+++ client/vue.config.js
@@ -1,4 +1,8 @@
 module.exports = {
   transpileDependencies: ['vuetify'],
 	outputDir: '../server/src/public',
+	publicPath: 'https://umk.coko38.ru',
+	devServer: {
+    proxy: 'http://localhost:3000'
+  },
 }
