feccc4f172f06d036ad6ae04de87c1aa1695d0e9
alexey.tsurov <alexey.tsurov@acmecryptocorp.io>
2022-06-25 21:36:32 +0300

    feat: build

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,8 +1,8 @@
 module.exports = {
 	transpileDependencies: ['vuetify'],
 	outputDir: '../server/src/public',
-	// publicPath: 'https://umk.coko38.ru',
-	devServer: {
-		proxy: 'http://localhost:3000',
-	},
+	publicPath: 'https://umk.coko38.ru',
+	// devServer: {
+	// 	proxy: 'http://localhost:3000',
+	// },
 }
